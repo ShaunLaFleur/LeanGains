7e442bc89fe297bd61104b935accdc3799fe34cc
Shaun LaFleur <shaunlafleur.kj@gmail.com>
2016-12-23 15:43:03 -0600

    Misc

--- temmp.js
+++ temmp.js
@@ -1,27 +1,28 @@
-function setStuff(a) {
+var setStuff = function(a) {
 	switch(a) {
-		case mySex:
+		case "mySex":
 			mySex = parseInt(document.getElementById("mysex").value);
 			setCals();
 			break;
-		case restday:
+		case "restday":
 			restPerc = parseFloat(document.getElementById("restperc").value);
 			setCals();
 			break;
-		case workday:
+		case "workday":
 			workPerc = parseFloat(document.getElementById("workperc").value);
 			setCals();
 			break;
-		case mysplit:
+		case "mysplit":
 			myWorkdays = parseInt(document.getElementById("split").value);
 			setCals();
 			break;
-		case activity:
+		case "activity":
 			myActivity = parseFloat(document.getElementById("activity").value);
 			setCals();
 			break;
-		case measure: 
+		case "measure": 
 			myMeasurement = parseInt(document.getElementById("measurement").value);
+			break;
 		default:
 			break;
 	}
